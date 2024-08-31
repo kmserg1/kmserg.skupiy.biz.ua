77b776136603cc245222f8feb8fb4f207be96035
Serhii Shatnii <kmserg@gmail.com>
2024-08-31 08:58:41 +0300

    First

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  // Input directory: src
+  // Output directory: _site
+  // Filters
+  const dateFilter = require('./src/filters/date-filter.js');
+  const timeFilter = require('./src/filters/time-filter.js');
+  const w3DateFilter = require('./src/filters/w3-date-filter.js');
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy('./src/images/');
+  eleventyConfig.addPassthroughCopy("*.html");
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addFilter('dateFilter', dateFilter);
+  eleventyConfig.addFilter('timeFilter', timeFilter);
+  eleventyConfig.addFilter('w3DateFilter', w3DateFilter);
+}

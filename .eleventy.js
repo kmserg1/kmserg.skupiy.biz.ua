5e3a979d4b3fd8882ba2927b80e770886583169d
Serhii Shatnii <kmserg@gmail.com>
2025-06-24 09:57:17 +0300

    add humor

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,25 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('dateFilter', dateFilter);
   eleventyConfig.addFilter('timeFilter', timeFilter);
   eleventyConfig.addFilter('w3DateFilter', w3DateFilter);
+
+
+  eleventyConfig.addCollection("anyTag", function(collectionApi) {
+    const tagsToMatch = ["humors", "it-posts", "commons"];
+
+      return collectionApi.getAll().filter(
+	item => {
+		const itemTags = Array.isArray(item.data.tags) ? item.data.tags : [item.data.tags];
+		// Перевіряємо, чи є хоча б один спільний тег
+		return itemTags.some(tag => tagsToMatch.includes(tag))
+		}
+     )
+  .sort((a, b) => b.date - a.date)  // сортування за датою у спадному порядку
+  .slice(0, 6);  // обмеження кількості елементів;
+
+  });
+
+
+
   eleventyConfig.addCollection("tags", function(collection) {
         let tags = new Set();
         collection.getAll().forEach(item => {
